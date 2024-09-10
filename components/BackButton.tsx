ba0d51db53a7b4441edf43ee9acb9e424cf94d23
Felix <felixgohh08@gmail.com>
2024-09-10 17:01:28 +0700

    remove left icon import

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ArrowLeftCircleIcon } from '@heroicons/react/24/outline';
 import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 import { usePathname, useRouter } from 'next/navigation';
 
